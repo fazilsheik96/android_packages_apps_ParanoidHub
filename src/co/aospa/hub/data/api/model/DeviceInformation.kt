Herna1994/ComposeParanoidHub

Paranoid Android Hub based in Compose initial push
Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>

app/src/main/java/co/aospa/paranoidhub/data/api/model/DeviceInformation.kt
@@ -1,0 +1,12 @@
+package co.aospa.paranoidhub.data.api.model
+
+data class DeviceInformation (
+    val codename: String?,
+    val paranoidAndroidVersion: String?,
+    val paranoidAndroidFlavor: Flavor?,
+    val paranoidAndroidNumber: String?,
+    val isABDevice: Boolean?,
+    val buildDate: String?,
+    val androidVersion: String?,
+    val securityPatch: String?
+)

app/src/main/java/co/aospa/paranoidhub/data/api/model/UpdateInformation.kt
@@ -1,0 +1,7 @@
+package co.aospa.paranoidhub.data.api.model
+
+data class Update (
+    val paranoidAndroidVersion: String?,
+    val downloadLink: String?,
+    val deviceChangelog: String?,
+)

app/src/main/java/co/aospa/paranoidhub/ui/components/LocalUpdateBottomSheet.kt
@@ -1,0 +1,81 @@
+package co.aospa.paranoidhub.ui.components
+
+import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import co.aospa.paranoidhub.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LocalUpdateBottomSheet(
+    showBottomSheetState: MutableState<Boolean>,
+    filePickerLauncher: ActivityResultLauncher<Intent>,
+) {
+    val sheetState = rememberModalBottomSheetState()
+
+    ModalBottomSheet(
+        windowInsets = WindowInsets(bottom = 0.dp),
+        onDismissRequest = {
+            showBottomSheetState.value = false
+        },
+        sheetState = sheetState
+    ) {
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(bottom = 8.dp),
+            text = stringResource(R.string.local_update),
+            style = MaterialTheme.typography.titleLarge
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(48.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.local_update_title),
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.padding(vertical = 8.dp))
+
+            Text(
+                text = stringResource(R.string.local_update_summary),
+                style = MaterialTheme.typography.titleSmall,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.padding(vertical = 16.dp))
+
+            Button(
+                onClick = {
+                    val intent = Intent(Intent.ACTION_GET_CONTENT)
+                        .setType("application/zip")
+                        .addCategory(Intent.CATEGORY_OPENABLE)
+                    filePickerLauncher.launch(intent)
+                },
+            ) {
+                Text(text = stringResource(R.string.install_local_update))
+            }
+        }
+    }
+}

app/src/main/java/co/aospa/paranoidhub/ui/theme/Type.kt
@@ -1,0 +1,34 @@
+package co.aospa.paranoidhub.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+)

app/src/main/java/co/aospa/paranoidhub/utils/DeviceUtils.kt
@@ -1,0 +1,61 @@
+package co.aospa.paranoidhub.utils
+
+import android.os.Build
+import android.os.SystemProperties
+import co.aospa.paranoidhub.GlobalConstants
+import co.aospa.paranoidhub.data.api.model.Flavor
+
+class DeviceUtils {
+
+    fun getCodename(): String {
+        return SystemProperties.get(GlobalConstants.PROP_DEVICE)
+    }
+
+    fun getParanoidAndroidVersion(): String {
+        val flavor = getCurrentVariant()
+        val version = getCurrentVersion()
+        val number = getCurrentNumber()
+
+        return if (flavor == Flavor.Stable) {
+            "$version $number"
+        } else {
+            "$version $flavor"
+        }
+    }
+
+    fun getCurrentVersion(): String {
+        return SystemProperties.get(GlobalConstants.PROP_VERSION_MAJOR)
+    }
+
+    fun getCurrentVariant(): Flavor {
+        val variant = SystemProperties.get(GlobalConstants.PROP_BUILD_TYPE)
+        val flavorMap = mapOf(
+            "Release" to Flavor.Stable,
+            "Beta" to Flavor.Beta,
+            "Alpha" to Flavor.Alpha,
+            "Unofficial" to Flavor.Unofficial
+        )
+
+        return flavorMap[variant] ?: Flavor.Unofficial
+    }
+
+    fun getCurrentNumber(): String {
+        return SystemProperties.get(GlobalConstants.PROP_VERSION_MINOR)
+    }
+
+    fun getAndroidVersion(): String {
+        return Build.VERSION.RELEASE
+    }
+
+    fun getSpl(): String {
+        return Build.VERSION.SECURITY_PATCH
+    }
+
+    fun isABDevice() : Boolean {
+        return SystemProperties.getBoolean(GlobalConstants.PROP_AB_DEVICE, false)
+    }
+
+    fun getBuildDate(): String {
+        return Build.TIME.toString()
+    }
+}

app/src/main/java/co/aospa/paranoidhub/utils/NotificationUtils.kt
@@ -1,0 +1,111 @@
+package co.aospa.paranoidhub.utils
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import co.aospa.paranoidhub.R
+import co.aospa.paranoidhub.data.api.methods.getdeviceinformation.Updates
+import co.aospa.paranoidhub.data.api.model.State
+
+object NotificationUtils {
+
+    private val notificationManager: NotificationManager? = null
+
+    fun showNotification(context: Context, update: Updates, state: State = State.NONE, installationProgress: Int = 0) {
+        val isStable = UpdateUtils.isStable(update.buildType)
+
+        val notification = NotificationCompat.Builder(context, "ParanoidHub Update")
+            .setSmallIcon(R.drawable.logo_dark)
+            .setContentTitle(
+                if (state == State.DOWNLOADING) {
+                    if (isStable) {
+                        context.getString(R.string.downloading_update_notification_title, update.version, update.versionCode)
+                    } else {
+                        context.getString(R.string.downloading_update_notification_title, update.version, update.buildType)
+                    }
+                } else {
+                    if (isStable) {
+                        context.getString(R.string.available_to_download_stable, update.version, update.versionCode)
+                    } else {
+                        context.getString(R.string.available_to_download, update.version, update.buildType)
+                    }
+                }
+            )
+            .setContentText(
+                if (state == State.DOWNLOADING) {
+                    ""
+                } else {
+                    context.getString(R.string.downloading_update_notification_summary)
+                })
+            .setOngoing(true)
+            .setOnlyAlertOnce(true)
+            .setAutoCancel(false)
+
+        if (state == State.READY_TO_INSTALL) {
+            val installIntent = Intent("DOWNLOAD")
+            val installPendingIntent = PendingIntent.getBroadcast(
+                context,
+                1,
+                installIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            notification.addAction(0, context.getString(R.string.install_action), installPendingIntent)
+        }
+
+        if (state == State.DOWNLOADING) {
+            val cancelIntent = Intent("CANCEL_DOWNLOAD")
+            val cancelPendingIntent = PendingIntent.getBroadcast(
+                context,
+                1,
+                cancelIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+            notification.addAction(0, context.getString(R.string.cancel_action), cancelPendingIntent)
+        }
+
+        if (state == State.DOWNLOADING || state == State.INSTALLING) {
+            notification.setProgress(100, installationProgress * 2, false)
+        }
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        val notificationId = 1
+        val channel = NotificationChannel(
+            "ParanoidHub Update",
+            "Update Notifications",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(channel)
+
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+
+        // Notify with a unique ID to update or replace the existing notification
+        notificationManager.notify(notificationId, notification.build())
+    }
+
+    fun cancelNotification(context: Context) {
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.cancel(1)
+    }
+}

fazilsheik96/android_packages_apps_ParanoidHub

src/co/aospa/hub/data/api/model/DeviceInformation.kt
@@ -1,0 +1,12 @@
+package co.aospa.hub.data.api.model
+
+data class DeviceInformation (
+    val codename: String?,
+    val paranoidAndroidVersion: String?,
+    val paranoidAndroidFlavor: Flavor?,
+    val paranoidAndroidNumber: String?,
+    val isABDevice: Boolean?,
+    val buildDate: String?,
+    val androidVersion: String?,
+    val securityPatch: String?
+)

src/co/aospa/hub/data/api/model/UpdateInformation.kt
@@ -1,0 +1,7 @@
+package co.aospa.hub.data.api.model
+
+data class Updates (
+    val paranoidAndroidVersion: String?,
+    val downloadLink: String?,
+    val deviceChangelog: String?,
+)

src/co/aospa/hub/ui/components/LocalUpdateBottomSheet.kt
@@ -1,0 +1,80 @@
+package co.aospa.hub.ui.components
+
+import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import co.aospa.hub.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LocalUpdateBottomSheet(
+    showBottomSheetState: MutableState<Boolean>,
+    filePickerLauncher: ActivityResultLauncher<Intent>,
+) {
+    val sheetState = rememberModalBottomSheetState()
+
+    ModalBottomSheet(
+        windowInsets = WindowInsets(bottom = 0.dp),
+        onDismissRequest = {
+            showBottomSheetState.value = false
+        },
+        sheetState = sheetState
+    ) {
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(bottom = 8.dp),
+            text = stringResource(R.string.local_update),
+            style = MaterialTheme.typography.titleLarge
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(48.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.local_update_title),
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.padding(vertical = 8.dp))
+
+            Text(
+                text = stringResource(R.string.local_update_summary),
+                style = MaterialTheme.typography.titleSmall,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.padding(vertical = 16.dp))
+
+            Button(
+                onClick = {
+                    val intent = Intent(Intent.ACTION_GET_CONTENT)
+                        .setType("application/zip")
+                        .addCategory(Intent.CATEGORY_OPENABLE)
+                    filePickerLauncher.launch(intent)
+                },
+            ) {
+                Text(text = stringResource(R.string.install_local_update))
+            }
+        }
+    }
+}

src/co/aospa/hub/ui/theme/Type.kt
@@ -1,0 +1,34 @@
+package co.aospa.hub.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+)

src/co/aospa/hub/utils/DeviceUtils.kt
@@ -1,0 +1,61 @@
+package co.aospa.hub.utils
+
+import android.os.Build
+import android.os.SystemProperties
+import co.aospa.hub.GlobalConstants
+import co.aospa.hub.data.api.model.Flavor
+
+class DeviceUtils {
+
+    fun getCodename(): String {
+        return SystemProperties.get(GlobalConstants.PROP_DEVICE)
+    }
+
+    fun getParanoidAndroidVersion(): String {
+        val flavor = getCurrentVariant()
+        val version = getCurrentVersion()
+        val number = getCurrentNumber()
+
+        return if (flavor == Flavor.Stable) {
+            "$version $number"
+        } else {
+            "$version $flavor"
+        }
+    }
+
+    private fun getCurrentVersion(): String {
+        return SystemProperties.get(GlobalConstants.PROP_VERSION_MAJOR)
+    }
+
+    fun getCurrentVariant(): Flavor {
+        val variant = SystemProperties.get(GlobalConstants.PROP_BUILD_TYPE)
+        val flavorMap = mapOf(
+            "Release" to Flavor.Stable,
+            "Beta" to Flavor.Beta,
+            "Alpha" to Flavor.Alpha,
+            "Unofficial" to Flavor.Unofficial
+        )
+
+        return flavorMap[variant] ?: Flavor.Unofficial
+    }
+
+    fun getCurrentNumber(): String {
+        return SystemProperties.get(GlobalConstants.PROP_VERSION_MINOR)
+    }
+
+    fun getAndroidVersion(): String {
+        return Build.VERSION.RELEASE
+    }
+
+    fun getSpl(): String {
+        return Build.VERSION.SECURITY_PATCH
+    }
+
+    fun isABDevice() : Boolean {
+        return SystemProperties.getBoolean(GlobalConstants.PROP_AB_DEVICE, false)
+    }
+
+    fun getBuildDate(): String {
+        return Build.TIME.toString()
+    }
+}

src/co/aospa/hub/utils/NotificationUtils.kt
@@ -1,0 +1,110 @@
+package co.aospa.hub.utils
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import co.aospa.hub.R
+import co.aospa.hub.data.api.methods.getdeviceinformation.GetDeviceInformationResponse
+import co.aospa.hub.data.api.model.State
+
+object NotificationUtils {
+
+    private val notificationManager: NotificationManager? = null
+
+    fun showNotification(context: Context, update: GetDeviceInformationResponse.Update, state: State = State.NONE, installationProgress: Int = 0) {
+        val isStable = UpdateUtils.isStable(update.build_type)
+
+        val notification = NotificationCompat.Builder(context, "ParanoidHub Updates")
+            .setSmallIcon(R.drawable.logo_dark)
+            .setContentTitle(
+                if (state == State.DOWNLOADING) {
+                    if (isStable) {
+                        context.getString(R.string.downloading_update_notification_title, update.version, update.version_code)
+                    } else {
+                        context.getString(R.string.downloading_update_notification_title, update.version, update.build_type)
+                    }
+                } else {
+                    if (isStable) {
+                        context.getString(R.string.available_to_download_stable, update.version, update.version_code)
+                    } else {
+                        context.getString(R.string.available_to_download, update.version, update.build_type)
+                    }
+                }
+            )
+            .setContentText(
+                if (state == State.DOWNLOADING) {
+                    ""
+                } else {
+                    context.getString(R.string.downloading_update_notification_summary)
+                })
+            .setOngoing(true)
+            .setOnlyAlertOnce(true)
+            .setAutoCancel(false)
+
+        if (state == State.READY_TO_INSTALL) {
+            val installIntent = Intent("DOWNLOAD")
+            val installPendingIntent = PendingIntent.getBroadcast(
+                context,
+                1,
+                installIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            notification.addAction(0, context.getString(R.string.install_action), installPendingIntent)
+        }
+
+        if (state == State.DOWNLOADING) {
+            val cancelIntent = Intent("CANCEL_DOWNLOAD")
+            val cancelPendingIntent = PendingIntent.getBroadcast(
+                context,
+                1,
+                cancelIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+            notification.addAction(0, context.getString(R.string.cancel_action), cancelPendingIntent)
+        }
+
+        if (state == State.DOWNLOADING || state == State.INSTALLING) {
+            notification.setProgress(100, installationProgress * 2, false)
+        }
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        val notificationId = 1
+        val channel = NotificationChannel(
+            "ParanoidHub Update",
+            "Update Notifications",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(channel)
+
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+
+        // Notify with a unique ID to update or replace the existing notification
+        notificationManager.notify(notificationId, notification.build())
+    }
+
+    fun cancelNotification(context: Context) {
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.cancel(1)
+    }
+}